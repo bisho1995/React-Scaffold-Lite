1516fad9f7008373cd0cbea6f31b022bbea20dd5
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-01-07 09:27:06 +0530

    feat: added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const path = require("path");
 
 module.exports = {
   entry: path.join(__dirname, "index.js"),
-  mode: "production",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: false,
+    port: 9000,
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
