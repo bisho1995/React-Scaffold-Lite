ee43dc1957f26b847312023f9cb8fdc62ce11145
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-01-16 17:38:29 +0530

    feat: added path alias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "@/app.css";
 import "antd/dist/antd.css";
 
 export default class App extends React.PureComponent {
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   devtool: "source-map",
   module: {
     rules: [
